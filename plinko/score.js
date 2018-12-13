7f69598b5eef09aa83e8f71983288df5f07c056d
boyang1020 <boyang1020@gmail.com>
2018-12-13 12:08:59 -0800

    testing the algorithm

--- plinko/score.js
+++ plinko/score.js
@@ -1,27 +1,29 @@
 const outputs = [];
+const predictionPoint = 400;
+const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
-  outputs.push([dropPosition, bounciness, size, bucketLabel]);
-
-  console.log(outputs);
+	// Ran every time a balls drops into a bucket
+	outputs.push([dropPosition, bounciness, size, bucketLabel]);
 
 }
 
 function runAnalysis() {
-  _.chain(outputs)
-  .map(row => [distance(row[0]), row[3]])
-  .sortBy(row =>[0])
-  .slice(0, k)
-  .countBy(row => row[1])
-  .toPairs()
-  .sortBy(row => row[1])
-  .last()
-  .first()
-  .parseInt()
-  .value()
+const bucket = _.chain(outputs)
+		.map(row => [distance(row[0]), row[3]])
+		.sortBy(row => [0])
+		.slice(0, k)
+		.countBy(row => row[1])
+		.toPairs()
+		.sortBy(row => row[1])
+		.last()
+		.first()
+		.parseInt()
+    .value();
+    
+    console.log('your point will probably fall into', bucket) ;
 }
 
 function distance(point) {
-  return Math.abs(point - predictionPoint);
+	return Math.abs(point - predictionPoint);
 }
