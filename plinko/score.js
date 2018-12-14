78cbf0c2a59c36ee793e53cedc9160de2f1e893e
boyang1020 <boyang1020@gmail.com>
2018-12-13 22:00:47 -0800

    .

--- plinko/score.js
+++ plinko/score.js
@@ -2,43 +2,41 @@ const outputs = [];
 const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-	// Ran every time a balls drops into a bucket
-	outputs.push([dropPosition, bounciness, size, bucketLabel]);
-
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
   const [testSet, trainingSet] = splitDataset(outputs, 10);
-  
+
   for (let i = 0; i < testSet.length; i++) {
     const bucket = knn(trainingSet, testSet[i][0]);
-    console.log(bucket);
+    console.log(bucket, testSet[i][3]);
   }
 }
 
 function knn(data, point) {
   return _.chain(data)
-  .map(row => [distance(row[0], point), row[3]])
-  .sortBy(row => [0])
-  .slice(0, k)
-  .countBy(row => row[1])
-  .toPairs()
-  .sortBy(row => row[1])
-  .last()
-  .first()
-  .parseInt()
-  .value();
+    .map(row => [distance(row[0], point), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
 }
 
 function distance(pointA, pointB) {
-	return Math.abs(pointA - pointB);
+  return Math.abs(pointA - pointB);
 }
 
 function splitDataset(data, testCount) {
   const shuffled = _.shuffle(data);
 
   const testSet = _.slice(shuffled, 0, testCount);
-  const trainingSet = _. slice(shuffled, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
 
   return [testSet, trainingSet];
 }
