3f28025cb0583ff1680ee7b642c894ff5f0c0db9
boyang1020 <boyang1020@gmail.com>
2019-01-02 18:31:14 -0800

    update node

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -1,8 +1,19 @@
-require('@tensorflow/tfjs-node');
-const tf = require('@tensorflow/tfs');
+const tf = require('@tensorflow/tfjs');
+const loadCSV = require('./load-csv');
+
+
+let { features, labels, testFeatures, testLabels} = loadCSV('kc_house_data.csv', {
+    shuffle: true,
+    splitTest: 10,
+    dataColumns: ['lat', 'long'],
+    labelColumns: ['price']
+});
+
+console.log(testFeatures);
+console.log(testLabels);
 
 const predictionPoint = tf.tensor([]);
-
+const k = 2;
 features
     .sub(predictionPoint)
     .pow(2)
