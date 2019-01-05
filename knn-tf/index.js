720d536cc9c647694949cbfddc15eaaa97dc938b
boyang1020 <boyang1020@gmail.com>
2019-01-05 01:33:37 -0800

    apply standardization

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -3,34 +3,39 @@ const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
 
 function knn(features, labels, predictionPoint, k) {
-    return features
-    .sub(predictionPoint)
-    .pow(2)
-    .sum(1)
-    .pow(0.5)
-    .expandDims(1)
-    .concat(labels, 1)
-    .unstack()
-    .sort((a, b)=> a.get(0) > b.get(0) ? 1 : -1)
-    .slice(0, k)
-    .reduce((acc, pair) => acc + pair.get(1), 0)/ k;
+    const { mean, variance } = tf.moments(features, 0);
 
+    const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(0.5));
+    
+	return (
+        features
+            .sub(mean)
+            .div(variance.pow(0.5))
+			.sub(scaledPrediction)
+			.pow(2)
+			.sum(1)
+			.pow(0.5)
+			.expandDims(1)
+			.concat(labels, 1)
+			.unstack()
+			.sort((a, b) => (a.get(0) > b.get(0) ? 1 : -1))
+			.slice(0, k)
+			.reduce((acc, pair) => acc + pair.get(1), 0) / k
+	);
 }
 
-let { features, labels, testFeatures, testLabels} = loadCSV('kc_house_data.csv', {
-    shuffle: true,
-    splitTest: 10,
-    dataColumns: ['lat', 'long'],
-    labelColumns: ['price']
+let { features, labels, testFeatures, testLabels } = loadCSV('kc_house_data.csv', {
+	shuffle: true,
+	splitTest: 10,
+	dataColumns: ['lat', 'long'],
+	labelColumns: ['price'],
 });
 
 features = tf.tensor(features);
 labels = tf.tensor(labels);
 
 testFeatures.forEach((testPoint, i) => {
-    const result = knn(features, labels, tf.tensor(testPoint), 10);
-    const err = (testLabels[i][0] - result) /testLabels[i][0] * 100;
-    console.log('Guess', result, testLabels[0][0], err); 
+	const result = knn(features, labels, tf.tensor(testPoint), 10);
+	const err = ((testLabels[i][0] - result) / testLabels[i][0]) * 100;
+	console.log('Guess', result, testLabels[0][0], err);
 });
-
-const { mean, variance } = tf.moments(numbers);
