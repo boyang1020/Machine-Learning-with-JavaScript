eababf989cd5642500fbb078942fb95d40291217
boyang1020 <boyang1020@gmail.com>
2019-01-08 00:00:36 -0800

    default algorithm options

--- regressions/linear-regression.js
+++ regressions/linear-regression.js
@@ -4,6 +4,9 @@ class LinearRegression {
     constructor(features, labels, options) {
         this.features = features;
         this.labels = labels;
+        
+        this.options = Object.assign({ learningRate: 0.1 }, options);
+
     }
 }
 
