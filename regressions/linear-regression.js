795cd6cd4c6eea828c951122fd71d6f3bc3fa588
boyang1020 <boyang1020@gmail.com>
2019-01-08 17:40:32 -0800

    formulating the training loop

--- regressions/linear-regression.js
+++ regressions/linear-regression.js
@@ -1,16 +1,17 @@
 const tf = require('@tensorflow/tfjs');
 
 class LinearRegression {
-    constructor(features, labels, options) {
-        this.features = features;
-        this.labels = labels;
-        
-        this.options = Object.assign({ learningRate: 0.1, iterations:  }, options);
+	constructor(features, labels, options) {
+		this.features = features;
+		this.labels = labels;
 
-    }
-    train() {
-
-    }
+		this.options = Object.assign({ learningRate: 0.1, iterations: 1000 }, options);
+	}
+	train() {
+		for (let i = 0; i < this.options.iterations; i++) {
+			this.gradientDescent();
+		}
+	}
 }
 
 module.exports = LinearRegression;
