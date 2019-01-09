0bed16999acb6a5aacff79809f6ac36374b31ac3
boyang1020 <boyang1020@gmail.com>
2019-01-08 18:32:23 -0800

    initial gradient descent implementation

--- regressions/linear-regression.js
+++ regressions/linear-regression.js
@@ -5,8 +5,21 @@ class LinearRegression {
 		this.features = features;
 		this.labels = labels;
 
-		this.options = Object.assign({ learningRate: 0.1, iterations: 1000 }, options);
-	}
+		this.options = Object.assign(
+            { learningRate: 0.1, iterations: 1000 }, 
+            options);
+        this.m = 0;
+        this.b = 0;
+    
+    }
+    
+    gradientDescent() {
+        const currentGuessesForMPG = this.features.map(row => {
+            return this.m * row[0] + this.b;
+        });
+        
+    }
+
 	train() {
 		for (let i = 0; i < this.options.iterations; i++) {
 			this.gradientDescent();
