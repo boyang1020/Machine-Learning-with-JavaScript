6e6e1efb73cd2038217811c77412481bc7067bcd
boyang1020 <boyang1020@gmail.com>
2019-01-09 23:52:42 -0800

    record mse history

--- regressions-tf/linear-regression.js
+++ regressions-tf/linear-regression.js
@@ -4,73 +4,85 @@ const _ = require('lodash');
 class LinearRegression {
 	constructor(features, labels, options) {
 		this.features = this.processFeatures(features);
-        this.labels = tf.tensor(labels);
-        
+		this.labels = tf.tensor(labels);
+		this.mseHistory = [];
 
-		this.options = Object.assign(
-            { learningRate: 0.1, iterations: 1000 }, 
-            options);
+		this.options = Object.assign({ learningRate: 0.1, iterations: 1000 }, options);
 
-       this.weights = tf.zeros([this.features.shape[1], 1]);
-    }
-    
-    gradientDescent() {
-        const currentGuesses = this.features.matMul(this.weights);
-        const differences = currentGuesses.sub(this.labels);
-        
-        const slopes = this.features
-        .transpose()
-        .matMul(differences)
-        .div(this.features.shape[0]);
+		this.weights = tf.zeros([this.features.shape[1], 1]);
+	}
 
-       this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
-    }
+	gradientDescent() {
+		const currentGuesses = this.features.matMul(this.weights);
+		const differences = currentGuesses.sub(this.labels);
+
+		const slopes = this.features
+			.transpose()
+			.matMul(differences)
+			.div(this.features.shape[0]);
+
+		this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
+	}
 
 	train() {
 		for (let i = 0; i < this.options.iterations; i++) {
-			this.gradientDescent();
+            this.gradientDescent();
+            this.recordMSE();
 		}
-    }
-    
-    test(testFeatures, testLabels) {
-        testFeatures = this.processFeatures(testFeatures);
-        testLabels = tf.tensor(testLabels);
+	}
 
-        
-        const predictions = testFeatures.matMul(this.weights);
+	test(testFeatures, testLabels) {
+		testFeatures = this.processFeatures(testFeatures);
+		testLabels = tf.tensor(testLabels);
 
-        const res = testLabels.sub(predictions)
-            .pow(2)
-            .sum()
-            .get();
-        const tot = testLabels.sub(testLabels.mean())
-            .pow(2)
-            .sum()
-            .get()
-        return 1 - res / tot;
-    }
+		const predictions = testFeatures.matMul(this.weights);
 
-    processFeatures(features) {
-        features = tf.tensor(features);
-        
-        if (this.mean && this.variance) {
-            features = features.sub(this.mean).div(this.variance.pow(0.5))
-        } else {
-            features = this.standardize(features);
-        }
+		const res = testLabels
+			.sub(predictions)
+			.pow(2)
+			.sum()
+			.get();
+		const tot = testLabels
+			.sub(testLabels.mean())
+			.pow(2)
+			.sum()
+			.get();
+		return 1 - res / tot;
+	}
 
-        features = tf.ones([features.shape[0], 1]).concat(features,1);
+	processFeatures(features) {
+		features = tf.tensor(features);
 
-        return features;
-    }
+		if (this.mean && this.variance) {
+			features = features.sub(this.mean).div(this.variance.pow(0.5));
+		} else {
+			features = this.standardize(features);
+		}
 
-    standardize(features) {
-        const { mean, variance } = tf.moments(features, 0);
+		features = tf.ones([features.shape[0], 1]).concat(features, 1);
 
-        this.mean = mean;
-        this.variance = variance;
-        return features.sub(mean).div(variance.pow(0.5));
-    }
+		return features;
+	}
+
+	standardize(features) {
+		const { mean, variance } = tf.moments(features, 0);
+
+		this.mean = mean;
+		this.variance = variance;
+		return features.sub(mean).div(variance.pow(0.5));
+	}
+
+	recordMSE() {
+		const mse = this.features
+			.matMul(this.weights)
+			.sub(this.labels)
+			.pow(2)
+			.sum()
+			.div(this.features.shape[0])
+			.get();
+
+		this.mseHistory.push(mse);
+	}
 }
 
 module.exports = LinearRegression;
