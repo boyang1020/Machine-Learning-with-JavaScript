b2727afafb64a6b6cea30dd513645d22e10015a3
boyang1020 <boyang1020@gmail.com>
2019-01-13 02:15:51 -0800

    memory

---
+++ Regression/multinominal-logistic-regression/memory.js
@@ -0,0 +1,13 @@
+const _ = require('loadash');
+
+const loadData = () => {
+    const radoms = _.range(0, 999999);
+
+    // return randoms;
+};
+
+const data = loadData();
+
+debugger;
+
+// node --inspect-brk memory.js
