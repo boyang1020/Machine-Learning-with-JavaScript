4331fe179c22fb34a68e566b3e5bcb01cd656d61
boyang1020 <boyang1020@gmail.com>
2019-01-13 22:57:07 -0800

    reading files from disk

---
+++ loadcsv/load-csv.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+function loadCSV(filename, options) {
+    const data = fs.readFileSync(filename, { encoding: 'utf-8'});
+    console.log(data);
+
+}
+
+loadCSV('data.csv');
