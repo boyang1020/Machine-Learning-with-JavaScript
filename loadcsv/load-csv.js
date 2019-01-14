35048ebfc7485af5d43dbc56ce3d1a34e77bb8e4
boyang1020 <boyang1020@gmail.com>
2019-01-13 23:29:24 -0800

    extracting data columns

--- loadcsv/load-csv.js
+++ loadcsv/load-csv.js
@@ -1,31 +1,47 @@
 const fs = require('fs');
-const _ = require('lodash')
+const _ = require('lodash');
 
-function loadCSV(filename, { converters = {} }) {
-    let data = fs.readFileSync(filename, { encoding: 'utf-8'});
-    data = data.split('\n').map(row => row.split(','));
-    data = data.map(row => _.dropRightWhile(row, val => val === ''));
+
+function extractColumns(data, columnNames) {
     const headers = _.first(data);
-    console.log(headers);
 
-    data = data.map((row, index) => {
-        if(index ===0 ) {
-            return row;
-        }
-        return row.map((element, index) => {
-            if (converters[headers[index]]) {
-                const converted = converters[headers[index]](element);
-                return _.isNaN(converted) ? element : converted;
-            }
-            const result = parseFloat(element);
-            return  _.isNaN(result) ? element : result;
-        });
+    const indexs = _.map(columnNames, column => headers.indexOf(column));
+    const extracted = _.map(data, row => _.pullAt(row, indexs));
+
+    return extracted;
+}
+
+function loadCSV(filename, { converters = {}, dataColumns = [], labelColumns = [] }) {
+	let data = fs.readFileSync(filename, { encoding: 'utf-8' });
+	data = data.split('\n').map(row => row.split(','));
+	data = data.map(row => _.dropRightWhile(row, val => val === ''));
+	const headers = _.first(data);
+
+	data = data.map((row, index) => {
+		if (index === 0) {
+			return row;
+		}
+		return row.map((element, index) => {
+			if (converters[headers[index]]) {
+				const converted = converters[headers[index]](element);
+				return _.isNaN(converted) ? element : converted;
+			}
+			const result = parseFloat(element);
+			return _.isNaN(result) ? element : result;
+		});
     });
-    console.log(data)
+    
+    let labels = extractColumns(data, labelColumns);
+    data = extractColumns(data, dataColumns);
+    data.shift();
+    labels.shift();
+	console.log(data);
 }
 
 loadCSV('data.csv', {
-    converters: {
-        passed: val => val === 'TRUE' ? 1 : 0
-    }
+	dataColumns: ['height', 'value'],
+	labelColumns: ['passed'],
+	converters: {
+		passed: val => (val === 'TRUE' ? 1 : 0),
+	},
 });
